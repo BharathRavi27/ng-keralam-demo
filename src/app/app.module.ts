a4921bf6ad4f5123cdb8b36eaea5d81a7c84e288
Bharath Ravi <bharathravi27@gmail.com>
2021-05-23 22:52:00 +0530

    v1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { V1Component } from './drawer/v1/v1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    V1Component
   ],
   imports: [
     BrowserModule,
